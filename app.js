146b90d3c883ab20b915843c6ba235a23b1c2c1f
Anthony Sims Jr <anthony.sims.226@gmail.com>
2017-06-16 12:32:03 -0400

    remove secret stuff; regenerate app keys and secrets

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ dotenv.load();
 
 // MAKE A TWITTER CLIENT
 var client = new Twitter({
-  // consumer_key: 'QslkNuaueFXU5leoIodLu6pN5',
-  // consumer_secret: 'VKbUM7yBkLyG7WfI6QHeOAYr0NNiyXpJGVlr4ynrrCaE44g8bQ',
-  // access_token_key: '1568363912-EkkB9m27GicjJTRf5LpvbH1MO6yoxIzFg1zCG5y',
-  // access_token_secret: 'qxsVvL2JeeWjYUzNI1p1hCJ7DKzMD5zcUfrKAXNl4qJlM'
-
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
@@ -34,11 +29,10 @@ client.get('statuses/user_timeline', {screen_name: 'twitterapi', count: 1}, func
   var reverseTweet = reverse(lastTweet); // Last tweet reversed.
   console.log("\nYour last tweet was: " + lastTweet);
   console.log("\nYour last tweet reversed is: " + reverseTweet);
-
-  // client.post('statuses/update', {status: reverseTweet},  function(error, tweet, response) {
-  // if(error) throw error;
-  // console.log(tweet);  // Tweet body.
-  // console.log(response);  // Raw response object.
-  // });
-
 });
+
+// client.post('statuses/update', {status: reverseTweet},  function(error, tweet, response) {
+// if(error) throw error;
+// console.log(tweet);  // Tweet body.
+// console.log(response);  // Raw response object.
+// });
